48ea9503aa7f8def4e2f07cf8245cc32784ea325
Mohammed Shabin <mohammedshabinm@gmail.com>
2021-02-05 23:30:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express=require('express');
+const bodyparser=require('body-parser');
+
+const getAllUsers = async(req,res)=>{
+    const {User}=require('./models/user');
+    const user = await User.find();
+     res.send(user);
+}
+
+const insertData=async(req,res)=>{
+  const {User}=require('./models/user');
+    const user=await User.create(req.body);
+    res.send(user);
+}
+
+const updateUser = async (req,res) => {
+    const {User} = require('./models/user');
+    const user = await User.findByIdAndUpdate({_id:req.params.id},req.body);
+    res.send(user);
+}
+const deleteUser = async (req,res) => {
+    const {User} = require('./models/user');
+    const user = await User.findByIdAndDelete({_id:req.params.id},req.body);
+    res.send(user);
+}
+async function startServer (){
+    const app=express();
+    app.use(bodyparser.json());
+    app.listen(3000,error =>{
+        if(error){
+            console.log("error",error);
+            process.exit(1);
+        }
+        console.log("server listening")
+    });
+    app.get('/users',getAllUsers);
+   app.post('/users',insertData);
+   app.put('/users/:id',updateUser);
+   app.post('/users/:id',deleteUser);
+
+    
+}
+startServer();
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const {mongoose,connection}=require('./index');
+const{Schema}=mongoose;
+
+const userSchema = new Schema(
+    {
+        name :{type:String, required :true,default:null},
+        profileImage :{type:String, required :true, default:null},
+        status :{type:String,required :true, default:null}
+    }
+    
+);
+
+const User=connection.model('User',userSchema);
+User.createCollection();
+module.exports={User};
